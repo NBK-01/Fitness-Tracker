6fc9b83bd6d04afcced1ae12de4d87821e7b6827
unknown <nayefkanaan03@gmail.com>
2021-11-05 06:19:37 +0300

    Adjusted route api import error

--- server.js
+++ server.js
@@ -3,27 +3,22 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/workout',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
-
 const app = express();
-
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
 
-app.use(require('./routes'));
+
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
