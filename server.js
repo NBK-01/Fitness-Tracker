fd2c24edf4554dadc0f0b47497c8a1eb78de8b08
unknown <nayefkanaan03@gmail.com>
2021-11-05 07:08:41 +0300

    final touches

--- server.js
+++ server.js
@@ -17,9 +17,10 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
   useCreateIndex: true
 });
 
-//...
+
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
